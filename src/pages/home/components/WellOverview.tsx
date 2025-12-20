cf11066e1eb03478f6e17b2cc4dc1666d7878102
AndriiS1 <andrii2353@gmail.com>
2025-12-20 13:49:21 +0200

    feat: well metadata component

--- src/pages/home/components/WellOverview.tsx
+++ src/pages/home/components/WellOverview.tsx
@@ -3,6 +3,7 @@ import type { Well } from "@/types/well";
 import { useEffect, useState } from "react";
 import CustomCalendar from "./CustomCalendar";
 import WellCharts from "./WellCharts";
+import WellMetadata from "./WellMetadata";
 
 interface ChartProps {
   deviceId: string;
@@ -11,12 +12,12 @@ interface ChartProps {
 }
 
 const WellOverview = ({ deviceId, from, to }: ChartProps) => {
-  const [deviceData, setDeviceData] = useState<Well | null>(null);
+  const [well, setWell] = useState<Well | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await WellApiService.getWell(deviceId);
-      setDeviceData(data);
+      setWell(data);
     };
 
     fetchData();
@@ -24,9 +25,19 @@ const WellOverview = ({ deviceId, from, to }: ChartProps) => {
 
   return (
     <div>
-      <CustomCalendar />
-      {deviceData?.assetName}
-      <WellCharts telemetry={[]} />
+      <div className="flex flex-row gap-4">
+        {well && <WellMetadata well={well} />}
+        <CustomCalendar />
+      </div>
+      <WellCharts
+        telemetry={[
+          { time: "08:00", value: 400 },
+          { time: "09:00", value: 300 },
+          { time: "10:00", value: 600 },
+          { time: "11:00", value: 800 },
+          { time: "12:00", value: 500 },
+        ]}
+      />
     </div>
   );
 };
